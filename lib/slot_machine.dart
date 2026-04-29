94cee1cc3534659db22c88ab367918c9778aaa45
alincqeek <pstartceva@yandex.ru>
2026-04-29 14:13:26 +0300

    Add coin protection, disabled button state and reset button

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -23,6 +23,12 @@ class _SlotMachineState extends State<SlotMachine> {
   var _message = '';
 
   void _spin() {
+    if (_coins <= 0) {
+      setState(() {
+        _message = 'Монеты закончились!😢';
+      });
+      return;
+    }
     setState(() {
       _slot1 =
           _symbols[_random.nextInt(_symbols.length)];
@@ -38,6 +44,16 @@ class _SlotMachineState extends State<SlotMachine> {
         _coins -= 1;
         _message = 'Попробуй еще раз 😔 -1 монета';
       }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _coins = 10;
+      _slot1 = 'assets/images/cherry.png';
+      _slot2 = 'assets/images/lemon.png';
+      _slot3 = 'assets/images/seven.png';
+      _message = '';
     });
   }
 
@@ -87,7 +103,7 @@ class _SlotMachineState extends State<SlotMachine> {
         ),
         SizedBox(height: 40),
         ElevatedButton(
-          onPressed: _spin,
+          onPressed: _coins > 0 ? _spin : null,
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.amber,
             padding: EdgeInsets.symmetric(
@@ -101,6 +117,17 @@ class _SlotMachineState extends State<SlotMachine> {
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.black,
+            ),
+          ),
+        ),
+        SizedBox(height: 12),
+        TextButton(
+          onPressed: _reset,
+          child: Text(
+            'Начать заново',
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 16,
             ),
           ),
         ),
